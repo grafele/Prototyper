a8000ebaebc37e3b2ff2675f3f702f2f50f14cda
Stefan Kofler <grafele@gmail.com>
2016-11-04 14:55:02 +0100

    add color picker

--- Prototyper/Classes/ImageAnnotationViewController.swift
+++ Prototyper/Classes/ImageAnnotationViewController.swift
@@ -19,8 +19,8 @@ class ImageAnnotationViewController: UIViewController {
     var delegate: ImageAnnotationViewControllerDelegate?
     
     fileprivate var jotViewController: JotViewController!
-    
     fileprivate var imageView: UIImageView!
+    fileprivate var colorButtons: [UIButton]!
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -30,6 +30,7 @@ class ImageAnnotationViewController: UIViewController {
         
         addImageView()
         addJotViewController()
+        addColorPicker()
         addBarButtonItems()
     }
     
@@ -44,8 +45,8 @@ class ImageAnnotationViewController: UIViewController {
         
         let views: [String: AnyObject] = ["topGuide": topLayoutGuide, "bottomGuide": bottomLayoutGuide, "imageView": imageView]
         
-        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "|-[imageView]-|", options: [], metrics: nil, views: views)
-        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-[imageView]-[bottomGuide]", options: [], metrics: nil, views: views)
+        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "|-30-[imageView]-30-|", options: [], metrics: nil, views: views)
+        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:[topGuide]-[imageView]-50-[bottomGuide]", options: [], metrics: nil, views: views)
         
         view.addConstraints(horizontalConstraints)
         view.addConstraints(verticalConstraints)
@@ -77,6 +78,108 @@ class ImageAnnotationViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtonPressed(_:)))
     }
     
+    fileprivate func addColorPicker() {
+        let colorPickerView = UIView() // full width, height: 60px
+        colorPickerView.backgroundColor = UIColor.white
+        colorPickerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(colorPickerView)
+        
+        let metrics = ["topSpacing": 8, "height": 50]
+        let views: [String: AnyObject] = ["bottomGuide": bottomLayoutGuide, "colorPickerView": colorPickerView]
+        
+        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "|-[colorPickerView]-|", options: [], metrics: metrics, views: views)
+        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:[colorPickerView(height)]-|", options: [], metrics: metrics, views: views)
+        
+        view.addConstraints(horizontalConstraints)
+        view.addConstraints(verticalConstraints)
+        
+        // color: red, yellow, blue, green, white, black
+        
+        let buttonSize: CGFloat = 35.0
+        
+        let redColorButton = UIButton()
+        redColorButton.backgroundColor = UIColor.red
+        prepareColorButton(redColorButton, buttonSize: buttonSize)
+        colorPickerView.addSubview(redColorButton)
+        
+        let spaceView1 = createSpaceView(in: colorPickerView)
+        
+        let yellowColorButton = UIButton()
+        yellowColorButton.backgroundColor = UIColor.yellow
+        prepareColorButton(yellowColorButton, buttonSize: buttonSize)
+        colorPickerView.addSubview(yellowColorButton)
+
+        let spaceView2 = createSpaceView(in: colorPickerView)
+
+        let cyanColorButton = UIButton()
+        cyanColorButton.backgroundColor = UIColor.cyan
+        prepareColorButton(cyanColorButton, buttonSize: buttonSize, selected: true)
+        colorPickerView.addSubview(cyanColorButton)
+
+        let spaceView3 = createSpaceView(in: colorPickerView)
+
+        let greenColorButton = UIButton()
+        greenColorButton.backgroundColor = UIColor.green
+        prepareColorButton(greenColorButton, buttonSize: buttonSize)
+        colorPickerView.addSubview(greenColorButton)
+
+        let spaceView4 = createSpaceView(in: colorPickerView)
+
+        let whiteColorButton = UIButton()
+        whiteColorButton.backgroundColor = UIColor.white
+        prepareColorButton(whiteColorButton, buttonSize: buttonSize)
+        colorPickerView.addSubview(whiteColorButton)
+
+        let spaceView5 = createSpaceView(in: colorPickerView)
+
+        let blackColorButton = UIButton()
+        blackColorButton.backgroundColor = UIColor.black
+        prepareColorButton(blackColorButton, buttonSize: buttonSize)
+        colorPickerView.addSubview(blackColorButton)
+        
+        let buttonMetrics = ["sideSpacing": 8, "size": buttonSize]
+        let buttonViews: [String: AnyObject] = ["redColorButton": redColorButton, "yellowColorButton": yellowColorButton, "cyanColorButton": cyanColorButton, "greenColorButton": greenColorButton, "whiteColorButton": whiteColorButton, "blackColorButton": blackColorButton, "spaceView1": spaceView1, "spaceView2": spaceView2, "spaceView3": spaceView3, "spaceView4": spaceView4, "spaceView5": spaceView5]
+        
+        let horizontalButtonConstraints = NSLayoutConstraint.constraints(withVisualFormat: "|-sideSpacing-[redColorButton(size)]-[spaceView1]-[yellowColorButton(size)]-[spaceView2(==spaceView1)]-[cyanColorButton(size)]-[spaceView3(==spaceView1)]-[greenColorButton(size)]-[spaceView4(==spaceView1)]-[whiteColorButton(size)]-[spaceView5(==spaceView1)]-[blackColorButton(size)]-sideSpacing-|", options: [], metrics: buttonMetrics, views: buttonViews)
+        
+        let heightConstraint1 = NSLayoutConstraint(item: redColorButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: buttonSize)
+        let heightConstraint2 = NSLayoutConstraint(item: yellowColorButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: buttonSize)
+        let heightConstraint3 = NSLayoutConstraint(item: cyanColorButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: buttonSize)
+        let heightConstraint4 = NSLayoutConstraint(item: greenColorButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: buttonSize)
+        let heightConstraint5 = NSLayoutConstraint(item: whiteColorButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: buttonSize)
+        let heightConstraint6 = NSLayoutConstraint(item: blackColorButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: buttonSize)
+        
+        let centerConstraint1 = NSLayoutConstraint(item: redColorButton, attribute: .centerY, relatedBy: .equal, toItem: colorPickerView, attribute: .centerY, multiplier: 1, constant: 0)
+        let centerConstraint2 = NSLayoutConstraint(item: yellowColorButton, attribute: .centerY, relatedBy: .equal, toItem: colorPickerView, attribute: .centerY, multiplier: 1, constant: 0)
+        let centerConstraint3 = NSLayoutConstraint(item: cyanColorButton, attribute: .centerY, relatedBy: .equal, toItem: colorPickerView, attribute: .centerY, multiplier: 1, constant: 0)
+        let centerConstraint4 = NSLayoutConstraint(item: greenColorButton, attribute: .centerY, relatedBy: .equal, toItem: colorPickerView, attribute: .centerY, multiplier: 1, constant: 0)
+        let centerConstraint5 = NSLayoutConstraint(item: whiteColorButton, attribute: .centerY, relatedBy: .equal, toItem: colorPickerView, attribute: .centerY, multiplier: 1, constant: 0)
+        let centerConstraint6 = NSLayoutConstraint(item: blackColorButton, attribute: .centerY, relatedBy: .equal, toItem: colorPickerView, attribute: .centerY, multiplier: 1, constant: 0)
+        
+        colorPickerView.addConstraints(horizontalButtonConstraints)
+        colorPickerView.addConstraints([heightConstraint1, heightConstraint2, heightConstraint3, heightConstraint4, heightConstraint5, heightConstraint6])
+        colorPickerView.addConstraints([centerConstraint1, centerConstraint2, centerConstraint3, centerConstraint4, centerConstraint5, centerConstraint6])
+        
+        colorButtons = [redColorButton, yellowColorButton, cyanColorButton, greenColorButton, whiteColorButton, blackColorButton]
+    }
+    
+    private func prepareColorButton(_ button: UIButton, buttonSize: CGFloat, selected: Bool = false) {
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = buttonSize/2.0
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.alpha = selected ? 1.0 : 0.4
+        button.addTarget(self, action: #selector(colorButtonPressed(_:)), for: .touchUpInside)
+    }
+    
+    private func createSpaceView(in superview: UIView) -> UIView {
+        let spaceView = UIView()
+        spaceView.translatesAutoresizingMaskIntoConstraints = false
+        spaceView.isHidden = true
+        superview.addSubview(spaceView)
+        return spaceView
+    }
+    
     // MARK: Actions
     
     func cancelButtonPressed(_ sender: AnyObject) {
@@ -86,5 +189,14 @@ class ImageAnnotationViewController: UIViewController {
     func saveButtonPressed(_ sender: AnyObject) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
         delegate?.imageAnnotated(jotViewController.draw(on: image))
-    }    
+    }
+    
+    func colorButtonPressed(_ colorButton: UIButton) {
+        for button in colorButtons {
+            button.alpha = 0.4
+        }
+
+        colorButton.alpha = 1.0
+        jotViewController.drawingColor = colorButton.backgroundColor
+    }
 }
